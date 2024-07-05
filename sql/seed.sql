ba83885a24d9557baf095308e09c1ce2c1161665
Dylan Legendre <dylanlegendre09@gmail.com>
2024-07-04 20:45:04 -0500

    working on database schema

--- sql/seed.sql
+++ sql/seed.sql
@@ -9,28 +9,32 @@
 -- This will create some initial data in the DB
 -- such as an initial admin user (you must reset the password),
 -- types, and other data that are necessary for the application
--- to function correctly.
+-- to function correctly. All other Seed data should be handled by the application.
 -- ############################
 
 -- // USER TYPES
-INSERT INTO public.user_types (id, name, key)
-VALUES (1, 'Admin', 'ut_admin')
+INSERT INTO public.user_types (name, key)
+VALUES ('Admin', 'ut_admin')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.user_types (id, name, key)
-VALUES (2, 'Mod', 'ut_mod')
+INSERT INTO public.user_types (name, key)
+VALUES ('Mod', 'ut_mod')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.user_types (id, name, key)
-VALUES (3, 'Player', 'ut_player')
+INSERT INTO public.user_types (name, key)
+VALUES ('Player', 'ut_player')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.user_types (name, key)
+VALUES ('System', 'ut_system')
 ON CONFLICT (id) DO NOTHING;
 
 -- // USERS
-INSERT INTO public.users (id, email, username, user_type_id)
-VALUES (1, 'snowlynxsoftware@gmail.com', 'MorpheusZero', 1)
+INSERT INTO public.users (email, username, user_type_id)
+VALUES ('snowlynxsoftware@gmail.com', 'MorpheusZero', 1)
 ON CONFLICT (id) DO NOTHING;
 
 -- // SHARDS
-INSERT INTO public.shards (id, name, description)
-VALUES (1, 'Hydrogen', 'The main persistent shard that all players start on')
+INSERT INTO public.shards (name, description)
+VALUES ('Hydrogen', 'The main persistent shard that all players start on')
 ON CONFLICT (id) DO NOTHING;
